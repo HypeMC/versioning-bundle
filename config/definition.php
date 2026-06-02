358b02d8445579d1a50fbcca51a8d461bb47b18b
HypeMC <hypemc@gmail.com>
2026-06-02 22:01:06 +0200

    Use AbstractBundle

---
+++ config/definition.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\Config\Definition\Configurator;
+
+use Bizkit\VersioningBundle\VCS\TaggingMode;
+use Bizkit\VersioningBundle\VCS\VCSHandlerInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
+return static function (DefinitionConfigurator $definition): void {
+    $definition->rootNode()
+        ->children()
+            ->scalarNode('parameter_prefix')
+                ->info('The prefix added to the version parameters.')
+                ->example('my_app')
+                ->cannotBeEmpty()
+                ->defaultValue('application')
+            ->end()
+            ->scalarNode('strategy')
+                ->info('The versioning strategy used.')
+                ->cannotBeEmpty()
+                ->defaultValue('incrementing')
+            ->end()
+
+            ->scalarNode('filename')
+                ->info('The name of the file containing the version information.')
+                ->cannotBeEmpty()
+                ->defaultValue('version')
+            ->end()
+            ->scalarNode('filepath')
+                ->info('The path to the file containing the version information.')
+                ->cannotBeEmpty()
+                ->defaultValue('%kernel.project_dir%/config')
+            ->end()
+
+            ->enumNode('format')
+                ->info('The format used for the version file.')
+                ->values(['yaml'])
+                ->cannotBeEmpty()
+                ->defaultValue('yaml')
+            ->end()
+
+            ->arrayNode('vcs')
+                ->info("Configuration for the VCS integration,\nset to false to disable the integration.")
+                ->addDefaultsIfNotSet()
+                ->beforeNormalization()
+                    ->ifTrue(static fn ($v): bool => \is_bool($v))
+                    ->then(static fn (bool $v): array => $v ? [] : ['handler' => null])
+                ->end()
+                ->children()
+                    ->scalarNode('handler')
+                        ->info("The handler used for the VCS integration,\nset to null to disable the integration.")
+                        ->defaultValue('git')
+                    ->end()
+
+                    ->scalarNode('commit_message')
+                        ->info('The message to use for the VCS commit.')
+                        ->cannotBeEmpty()
+                        ->defaultValue(VCSHandlerInterface::DEFAULT_MESSAGE)
+                    ->end()
+                    ->enumNode('tagging_mode')
+                        ->beforeNormalization()
+                            ->ifString()
+                            ->then(static fn ($v) => TaggingMode::tryFrom($v) ?? throw new InvalidConfigurationException(\sprintf(
+                                'Invalid tagging mode provided: expected one of "%s", got "%s".',
+                                implode('", "', array_map(static fn (TaggingMode $case) => $case->value, TaggingMode::cases())),
+                                $v,
+                            )))
+                        ->end()
+                        ->values(TaggingMode::cases())
+                        ->info("The mode for applying tags to version commits:\n- 'always': automatically add a tag without prompting\n- 'never': do not add a tag\n- 'ask': prompt before tagging when incrementing versions")
+                        ->cannotBeEmpty()
+                        ->defaultValue(TaggingMode::Ask)
+                    ->end()
+                    ->scalarNode('tag_message')
+                        ->info('The message to use for the VCS tag.')
+                        ->cannotBeEmpty()
+                        ->defaultValue(VCSHandlerInterface::DEFAULT_MESSAGE)
+                    ->end()
+
+                    ->scalarNode('name')
+                        ->info("The name used for the VCS commit information,\nset to null to use the default VCS configuration.")
+                        ->defaultNull()
+                    ->end()
+                    ->scalarNode('email')
+                        ->info("The email used for the VCS commit information,\nset to null to use the default VCS configuration.")
+                        ->defaultNull()
+                    ->end()
+
+                    ->scalarNode('path_to_executable')
+                        ->info("The path to the VCS executable,\nset to null for autodiscovery.")
+                        ->defaultNull()
+                    ->end()
+                ->end()
+            ->end()
+        ->end()
+    ;
+};
--- src/BizkitVersioningBundle.php
+++ src/BizkitVersioningBundle.php
@@ -4,12 +4,106 @@ declare(strict_types=1);
 
 namespace Bizkit\VersioningBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Bizkit\VersioningBundle\Reader\ReaderInterface;
+use Bizkit\VersioningBundle\Strategy\StrategyInterface;
+use Bizkit\VersioningBundle\VCS\VCSHandlerInterface;
+use Bizkit\VersioningBundle\Writer\WriterInterface;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class BizkitVersioningBundle extends Bundle
+final class BizkitVersioningBundle extends AbstractBundle implements CompilerPassInterface
 {
-    public function getPath(): string
+    private string $configuredFormat;
+    private string $configuredStrategy;
+    private ?string $configuredVCSHandler;
+
+    public function build(ContainerBuilder $container): void
     {
-        return \dirname(__DIR__);
+        $container->addCompilerPass($this); // todo
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->import(\dirname(__DIR__).'/config/definition.php');
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $configurator, ContainerBuilder $container): void
+    {
+        $this->configuredFormat = $config['format'];
+        $this->configuredStrategy = $config['strategy'];
+        $this->configuredVCSHandler = $config['vcs']['handler'];
+
+        $configurator->import(\dirname(__DIR__).'/config/services.php');
+
+        $filepath = $container->getParameterBag()->resolveValue($config['filepath']);
+        $file = $filepath.\DIRECTORY_SEPARATOR.$config['filename'].'.'.$config['format'];
+
+        $container->fileExists($file); // todo
+
+        $configurator->import($file, $config['format'], 'not_found');
+
+        $container->registerForAutoconfiguration(StrategyInterface::class)
+            ->addTag('bizkit_versioning.strategy');
+
+        $container->registerForAutoconfiguration(VCSHandlerInterface::class)
+            ->addTag('bizkit_versioning.vcs_handler');
+
+        $container->setParameter('.bizkit_versioning.parameter_prefix', $config['parameter_prefix']);
+        $container->setParameter('.bizkit_versioning.file', $file);
+
+        $container->setParameter('.bizkit_versioning.vcs_commit_message', $config['vcs']['commit_message']);
+        $container->setParameter('.bizkit_versioning.vcs_tag_message', $config['vcs']['tag_message']);
+        $container->setParameter('.bizkit_versioning.vcs_tagging_mode', $config['vcs']['tagging_mode']);
+        $container->setParameter('.bizkit_versioning.vcs_name', $config['vcs']['name']);
+        $container->setParameter('.bizkit_versioning.vcs_email', $config['vcs']['email']);
+        $container->setParameter('.bizkit_versioning.path_to_vcs_executable', $config['vcs']['path_to_executable']);
+    }
+
+    /**
+     * Needs to happen after {@see ResolveInstanceofConditionalsPass} & {@see ResolveClassPass}.
+     */
+    public function process(ContainerBuilder $container): void
+    {
+        $this->registerServiceAlias($container, 'bizkit_versioning.reader', 'format', $this->configuredFormat, ReaderInterface::class);
+        $this->registerServiceAlias($container, 'bizkit_versioning.writer', 'format', $this->configuredFormat, WriterInterface::class);
+
+        $this->registerServiceAlias($container, 'bizkit_versioning.strategy', 'alias', $this->configuredStrategy, StrategyInterface::class, true);
+
+        if (null !== $this->configuredVCSHandler) {
+            $this->registerServiceAlias($container, 'bizkit_versioning.vcs_handler', 'alias', $this->configuredVCSHandler, VCSHandlerInterface::class, true);
+        }
+    }
+
+    private function registerServiceAlias(
+        ContainerBuilder $container,
+        string $tag,
+        string $attribute,
+        string $configuredValue,
+        string $alias,
+        bool $fallbackToServiceId = false,
+    ): void {
+        $taggedServices = $container->findTaggedServiceIds($tag);
+
+        foreach ($taggedServices as $id => $tags) {
+            $value = $tags[0][$attribute] ?? ($fallbackToServiceId ? $id : null);
+
+            if ($configuredValue === $value) {
+                $container->setAlias($alias, new Alias($id, false));
+
+                return;
+            }
+        }
+
+        throw new InvalidArgumentException(\sprintf(
+            'Unknown configuration value "%s", there is no service with the tag "%s" and attribute "%s" with that value registered.',
+            $configuredValue,
+            $tag,
+            $attribute,
+        ));
     }
 }
